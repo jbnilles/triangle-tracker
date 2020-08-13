676db1c4eed95c356ffd44cfb89de4a97e587708
jbnilles <jbnilles24@gmail.com>
2020-08-13 15:13:59 -0700

    tell user result and refactor code for readability
    Co-authored-by: AJ Markow <alexanderjmarkow@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,17 @@ $(document).ready(function() {
     const num2 =parseInt ($("#num2").val());
     const num3 =parseInt ($("#num3").val());
     event.preventDefault();
-    //Equilateral
     if( num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1){
-      alert("Not a triangle!");
+      $('#answer').text('Not a triangle.');
     }
-    else if (num1 === num2 && num1 === num3) { //num1 === num2 && num1 === num3       num1 === num2 === num3
-      //$('#num1').show();
-      alert("Equilateral");
-    //Isosceles  1      3      3
-    } else if (num1 === num2 || num1 === num3 || num2 === num3) {
-      //$('#num2').show();
-      alert("Isosceles");
-    //Scalene
-    } else if (num1 !== num2 && num1 !== num3 && num2 !== num3){
-     //Not a Triangle  
-     alert("Scalene");
+    else if (num1 === num2 && num1 === num3) { 
+      $('#answer').text('Equilateral triangle.');
+    } 
+    else if (num1 === num2 || num1 === num3 || num2 === num3) {
+      $('#answer').text('Isosceles triangle.');
+    } 
+    else if (num1 !== num2 && num1 !== num3 && num2 !== num3){
+      $('#answer').text('Scalene triangle.');
     }
     else{
       alert("ERROR");
